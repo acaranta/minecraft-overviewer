5f55f4199936679f1d650f04b4a2571c71dc428b
Mitchell Skaggs <skaggsm333@gmail.com>
2020-04-07 22:47:08 -0500

    Disable shellcheck warnings when we actually want to split/glob arguments
    See the last note here: https://github.com/koalaman/shellcheck/wiki/SC2086
    
    > As always, this warning can be ignored on a case-by-case basis.
    > This is especially relevant when BASH may not be available for the array work around. For example, use in eval or in command options where script has total control of the variables...

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,19 +13,23 @@ echo "Using Client URL $CLIENT_URL."
 wget -N "${CLIENT_URL}" -O "${MINECRAFT_VERSION}.jar" -P "/home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/"
 
 if [ -n "$RCON_ARGS_PRE" ]; then
+  # shellcheck disable=SC2086
   rcon-cli $RCON_ARGS_PRE
 fi
 
 # Render the Map
 if [ "$RENDER_MAP" == "true" ]; then
+  # shellcheck disable=SC2086
   overviewer.py --config "$CONFIG_LOCATION" $ADDITIONAL_ARGS
 fi
 
 # Render the POI
 if [ "$RENDER_POI" == "true" ]; then
+  # shellcheck disable=SC2086
   overviewer.py --config "$CONFIG_LOCATION" --genpoi $ADDITIONAL_ARGS_POI
 fi
 
 if [ -n "$RCON_ARGS_POST" ]; then
+  # shellcheck disable=SC2086
   rcon-cli $RCON_ARGS_POST
 fi
